49a84d9dde483b6175d9c3a3b9b385bc7aaf8451
Vedant102 <84623867+Vedant102@users.noreply.github.com>
2022-10-16 12:29:35 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,3 +1,5 @@
+In computer science, binary search, also known as half-interval search, logarithmic search, or binary chop, is a search algorithm that finds the position of a target value within a sorted array. Binary search compares the target value to the middle element of the array
+
 #include <iostream>
 using namespace std;
 
