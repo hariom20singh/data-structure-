37a9cdc3100054c27a26d355146a79a1337b6eb9
abhay1822 <72266499+abhay1822@users.noreply.github.com>
2022-10-16 16:51:25 +0530

    Longest_Consecutive_Sequence.cpp
    Problem Statement: You are given an array of ‘N’ integers. You need to find the length of the longest sequence which contains the consecutive elements.

---
+++ ArrayQuestion/Longest_Consecutive_Sequence.cpp
@@ -0,0 +1,35 @@
+//Problem Statement: You are given an array of ‘N’ integers. You need to find the length of the longest sequence which contains the consecutive elements.
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int longestConsecutive(vector < int > & nums) {
+  set < int > hashSet;
+  for (int num: nums) {
+    hashSet.insert(num);
+  }
+
+  int longestStreak = 0;
+
+  for (int num: nums) {
+    if (!hashSet.count(num - 1)) {
+      int currentNum = num;
+      int currentStreak = 1;
+
+      while (hashSet.count(currentNum + 1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+
+      longestStreak = max(longestStreak, currentStreak);
+    }
+  }
+
+  return longestStreak;
+}
+int main() {
+  vector<int> arr{100,200,1,2,3,4};
+  int lon = longestConsecutive(arr);
+  cout << "The longest consecutive sequence is " << lon << endl;
+
+}
