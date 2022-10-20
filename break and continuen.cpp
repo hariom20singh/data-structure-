2c9d9b646feb8b056bbc0fd6b9c2b6a96214d1d0
udham31 <116259376+udham31@users.noreply.github.com>
2022-10-20 13:24:03 +0530

    hacktoberfest2022

---
+++ break and continuen.cpp
@@ -0,0 +1,24 @@
+/*#include <iostream>
+using namespace std;
+int main (){
+    for (int i=0;i<40;i++)
+    {
+        if (i==2){
+        break;
+      }
+    cout<<i<<endl;
+}
+   return 0;
+*/
+#include <iostream>
+using namespace std;
+int main (){
+    for (int i=0;i<40;i++)
+    {
+        if (i==2){
+     continue;
+      }
+     cout<<i<<endl;
+    }
+       return 0;
+}
