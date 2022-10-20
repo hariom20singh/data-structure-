72b562e298c4ae9fce5a79a5a55dcbf619e7fcf9
prateek-1211 <116022110+prateek-1211@users.noreply.github.com>
2022-10-20 22:16:50 +0530

    hactober 2022

--- main.cpp
+++ main.cpp
@@ -1,12 +1,50 @@
-#include <iostream>
 
+// In computer science, binary search, also known as half-interval search, logarithmic search, or binary chop, is a search algorithm that finds the position of a target value within a sorted array. Binary search compares the target value to the middle element of the array
+
+#include <iostream>
 using namespace std;
 
-int main() {
+int binary_search(int arr[], int n, int key)
+{
+    // implement binary search
+    int start = 0;
+    int end = n - 1;
 
-  int Example[2];
+    while (start <= end)
+    {
+        int mid = (end + start) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] >= key)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
 
-  Example[0] = 200;
-  Example[1] = 201;
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 89};
+    int n = sizeof(arr) / sizeof(int);
 
+    int key;
+    cin >> key;
+
+    int index = binary_search(arr, n, key);
+    if (index != -1)
+    {
+        cout << key << " is found at " << index << endl;
+    }
+    else
+    {
+        cout << key << " is not found!" << endl;
+    }
+    return 0;
 }
